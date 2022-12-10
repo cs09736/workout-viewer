15f18b08ff537eb532f5fcd825ef648936802366
Sam Cantwell <cs09736@gmail.com>
2022-12-10 23:03:53 +1100

    added coordinates to image position

--- workout-viewer.py
+++ workout-viewer.py
@@ -1,4 +1,31 @@
 import numpy
 
+UPPER_LATITUDE = -37.75
+LOWER_LATITUDE = -37.96
+UPPER_LONGITUDE = 145.15
+LOWER_LONGITUDE = 144.89
+# 36 km wide
+# 23 km tall
+# image size = 2300x3600 px
+
+IMG_WIDTH = 2300
+IMG_HEIGHT = 3600
+
+def coordinate_to_pixel(latitude, longitude):
+	degrees_wide = UPPER_LONGITUDE - LOWER_LONGITUDE
+	degrees_tall = UPPER_LATITUDE - LOWER_LATITUDE
+
+	latitude_offset = latitude - LOWER_LATITUDE
+	longitude_offset = longitude - LOWER_LONGITUDE
+
+	x_position = longitude_offset / degrees_wide
+	y_position = latitude_offset / degrees_tall
+	return (x_position, y_position)
+
+print(coordinate_to_pixel(UPPER_LATITUDE, UPPER_LONGITUDE))
+
+
+
+
 print("Hello world!")
 print("A second message")
